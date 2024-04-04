41f13eee7b773d9d244aca8ebce9048259c232f2
jrosenbergpc-dev <jrosenbergpc@gmail.com>
2024-04-04 16:59:11 -0400

    Updated Profile Manager a little bit, still working out data structure and class organization

--- Minecraft Modded Server Updater/Tools/ProfileManager.cs
+++ Minecraft Modded Server Updater/Tools/ProfileManager.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Minecraft_Modded_Server_Updater.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,28 @@ namespace Minecraft_Modded_Server_Updater.Tools
 {
     internal class ProfileManager
     {
+        private List<ServerProfile> _profiles;
+
+        public ProfileManager() 
+        {
+        
+        }
+
+        public List<ServerProfile> Profiles { get { return _profiles; } }
+
+        public void CreateNewProfile()
+        {
+
+        }
+
+        public void UpdateProfile()
+        { 
+        
+        }
+
+        public void DeleteProfile()
+        {
+
+        }
     }
 }
