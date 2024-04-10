81bea36cdbb04f7bfc753b9f1007fd2ea28dddb9
Jonathan Rosenberg <jonr@ilsny.com>
2024-04-10 01:38:26 -0400

    Added Images and basic UI User Controls

---
+++ Minecraft Modded Server Updater/UI/NavButton.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Minecraft_Modded_Server_Updater.UI
+{
+	/// <summary>
+	/// Interaction logic for NavButton.xaml
+	/// </summary>
+	public partial class NavButton : UserControl
+	{
+		public NavButton()
+		{
+			InitializeComponent();
+		}
+
+		private void Label_MouseEnter(object sender, MouseEventArgs e)
+		{
+
+        }
+
+		private void Label_MouseLeave(object sender, MouseEventArgs e)
+		{
+
+		}
+
+		private void Label_MouseDown(object sender, MouseButtonEventArgs e)
+		{
+
+		}
+
+		private void Label_MouseUp(object sender, MouseButtonEventArgs e)
+		{
+
+		}
+	}
+}
