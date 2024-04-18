b78d54b600cd551373e850bd7ac78af4e2df518c
Jonathan Rosenberg <jonr@ilsny.com>
2024-04-18 00:08:04 -0400

    might need to pivot to a different branch, seems like the production is getting pushed back over time. Added all new sections, added pages and support for the core frame in Main Window.

---
+++ Minecraft Modded Server Updater/Pages/Main.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Minecraft_Modded_Server_Updater.Pages
+{
+	/// <summary>
+	/// Interaction logic for Main.xaml
+	/// </summary>
+	public partial class Main : Page
+	{
+		public Main()
+		{
+			InitializeComponent();
+		}
+
+		private void AddButton_OnNavClicked(object sender, EventArgs e)
+		{
+
+        }
+
+		private void RemoveButton_OnNavClicked(object sender, EventArgs e)
+		{
+
+		}
+	}
+}
